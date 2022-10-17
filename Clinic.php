d190fb56360ef681a56eeff3e89fe174c09c4713
KirillSemenofff <kirillzelek@gmail.com>
2022-10-17 17:14:42 +0300

    test

---
+++ Clinic.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace src\Main;
+
+class Clinic
+{
+    protected array $arrayDoctors = [];
+    protected array $arrayPatients = [];
+    protected string $address;
+
+    public function __construct($address)
+    {
+        $this->address = $address;
+    }
+
+    public function setArrayDoctors($name, $surname){
+        $this->arrayDoctors[$surname] = $name;
+    }
+    public function setArrayPatients($name, $surname){
+        $this->arrayPatients[$surname] = $name;
+    }
+}
