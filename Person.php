c277e8cf888f838bdcf380f05dc3549ea52a89a9
KirillSemenofff <kirillzelek@gmail.com>
2022-10-20 16:47:04 +0300

    feat(property): add new property(/1)

--- Person.php
+++ Person.php
@@ -6,19 +6,21 @@ class Person
     protected string $surname;
     protected string $gender;
     protected string $person_id;
+    protected string $clinicAddress;
     protected int $age;
 
-    public function __construct($name, $surname, $gender, $age)
+    public function __construct($name, $surname, $gender, $age, $clinicAddress)
     {
         $this->name = $name;
         $this->surname = $surname;
         $this->gender = $gender;
         $this->age = $age;
+        $this->clinicAddress= $clinicAddress;
         $this->person_id = uniqid();
     }
 
     public function getFullInfo(): string
     {
-        return "{$this->name} " . "{$this->surname} " . "{$this->gender} " . "{$this->age} " . "{$this->person_id}";
+        return "{$this->name} " . "{$this->surname} " . "{$this->gender} " . "{$this->age} " . "{$this->person_id} " . "{$this->clinicAddress}";
     }
 }
