573a57592fa28c2211caf2de1b14e2d98e7a7c59
KirillSemenofff <kirillzelek@gmail.com>
2022-10-20 16:58:03 +0300

    feat(class): added Doctor class(#1)

--- Doctor.php
+++ Doctor.php
@@ -1,6 +1,15 @@
 <?php
 
-class Doctor
+class Doctor extends Person
 {
+    protected array $arrayDoctorPatients = [];
+    protected string $specialization;
 
+    protected function breatheCheck(): string
+    {
+        $rand = rand(0, 100);
+        if ($rand > 50) {
+            return "Дышите...";
+        } else return "Не дышите...";
+    }
 }
