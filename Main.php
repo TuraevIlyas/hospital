5ad5623f55ddb554751a75d1b09b407a68c99e68
korotkov-ai <r.korotkov@sxope.com>
2022-10-25 19:28:28 +0300

    feat(file): Add unfinished logic for interrelated interaction between classes (OCEAN-9999)

---
+++ Main.php
@@ -0,0 +1,15 @@
+<?php
+spl_autoload_register(function ($className){
+    include $className . '.php';
+});
+echo 'Welcome, head physician. Database available. Search:' . PHP_EOL;
+while(TRUE) {
+    $clinic = new Clinic();
+    $doctor = new Doctor();
+    $pacient = new Pacient();
+    readline();
+}
+
+//Pacient file non-existent
+//Doctor file has an error in __construct()
+//Pls fix
