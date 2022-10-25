cd81d659fecb9db119dc533518451c68a9ac9aa7
KirillSemenofff <kirillzelek@gmail.com>
2022-10-25 18:11:23 +0300

    refactor(property): change property name in class Person

--- Person.php
+++ Person.php
@@ -5,28 +5,27 @@ class Person
     protected string $name;
     protected string $surname;
     protected string $gender;
-    protected string $person_id;
+    protected string $personId;
     protected string $addressClinic;
     protected int $age;
 
-    public function __construct($name, $surname, $gender, $age, $addressClinic, $person_id)
+    public function __construct($name, $surname, $gender, $age, $addressClinic, $personId)
     {
         $this->name = $name;
         $this->surname = $surname;
         $this->gender = $gender;
         $this->age = $age;
         $this->addressClinic = $addressClinic;
-        $this->person_id = uniqid();
+        $this->personId = uniqid();
     }
 
     public function getFullInfo(): string
     {
-        return "$this->name " . "$this->surname " . "$this->gender " . "$this->age " . "$this->person_id";
+        return "$this->name " . "$this->surname " . "$this->gender " . "$this->age " . "$this->personId";
     }
 
 
     public function getPersonId(){
-        return $this->person_id;
+        return $this->personId;
     }
 }
-
