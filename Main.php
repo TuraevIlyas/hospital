aaa5abe418299c1782e6f6155c0a8d8d8ca2eae4
KirillSemenofff <kirillzelek@gmail.com>
2022-10-29 13:00:53 +0300

    feat(require): add require for functions(#9)

--- Main.php
+++ Main.php
@@ -1,14 +1,17 @@
 <?php
+
+require 'functions/Randomizer.php';
+require 'functions/FillInfo.php';
+
 spl_autoload_register(function ($className){
     include $className . '.php';
 });
+
 echo 'Welcome, head physician. Database available. Search:' . PHP_EOL;
 while(TRUE) {
     $clinic = new Clinic();
     $patient = new Patient();
     $input = readline();
-
-
 }
 
 //Doctor file has an error in __construct()
