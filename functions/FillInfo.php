2f5ea9444ad4b0c205b4e2f4f3bb0d45cda76b8b
Andrey Bagdasaryan <a.bagdasaryan@sxope.com>
2022-10-30 18:02:36 +0300

    feat(Fillinfo): added a statement for attaching patients to doctors (#11)

--- functions/FillInfo.php
+++ functions/FillInfo.php
@@ -8,14 +8,19 @@ require '..\Patient.php';
 
 function fillInfo(int $amountDoctors, int $amountPatients): void {
     for ($i=1; $i<=$amountDoctors; $i++){
-         $doctor = new Doctor(fillMembers());
-         Clinic::setArrayDoctors($doctor->getPersonId(), $doctor->getName(), $doctor->getSurname(), $doctor->getSpecialization());
+        $doctor = new Doctor(fillMembers());
+        Clinic::setArrayDoctors($doctor->getPersonId(), $doctor->getName(), $doctor->getSurname(), $doctor->getSpecialization());
     }
     for ($a=1; $a <= $amountPatients; $a++){
         $patient = new Patient(fillMembers());
         $patient->setDiseases();
         Clinic::setArrayPatients($patient->getPersonId(), $patient->getName(), $patient->getSurname());
+
+        $chainedPatient = array_rand(Clinic::getArrayPatients());
+
+        if (!isset($arrayPatients[$chainedPatient])) {
+            // TODO: link keys (patientId) amd values (doctorId) (to create a relations)
+        } else $a--;
     }
 }
 fillInfo(3, 13);
-print_r(Clinic::getArrayDoctors());
