70020a18e2a069aa84017d8dde64207c355fb6a1
rk <kutcko1.99@gmail.com>
2022-10-31 16:16:28 +0300

    feat(functions): incomplete logic add (#11)

--- functions/FillInfo.php
+++ functions/FillInfo.php
@@ -1,10 +1,19 @@
 <?php
 
+$dir = "..\..\hospital";
+$catalog = opendir($dir);
+while ($filename = readdir($catalog ))
+{
+    if(strpos($filename, '.php')){
+        $filename = $dir."/".$filename;
+        require($filename);
+    }
+
+}
+closedir($catalog);
+
 require 'Randomizer.php';
-require '..\Person.php';
-require '..\Doctor.php';
-require '..\Clinic.php';
-require '..\Patient.php';
+
 
 function fillInfo(int $amountDoctors, int $amountPatients): void {
     for ($i=1; $i<=$amountDoctors; $i++){
